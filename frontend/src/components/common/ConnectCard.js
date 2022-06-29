a471656edb412d5d871242913022b8abe366e65f
sohan1998 <sohan1998@gmail.com>
2022-06-28 23:48:36 -0700

    fix(frontend): store state variables in local storage

--- frontend/src/components/common/ConnectCard.js
+++ frontend/src/components/common/ConnectCard.js
@@ -53,8 +53,8 @@ export default function ConnectCard() {
             {/* <CardMedia component='img' height='194' image='/static/images/cards/paella.jpg' alt='Paella dish' /> */}
             <CardContent>
                 <Typography variant='body2' color='text.secondary'>
-                    This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along
-                    with the mussels, if you like.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </Typography>
             </CardContent>
             {/* <CardActions disableSpacing>
--- frontend/src/components/register/register.js
+++ frontend/src/components/register/register.js
@@ -1,38 +1,100 @@
 import React, { Component } from 'react';
 import { Button, Form, Container } from 'react-bootstrap';
 import './../common/button.css';
+import { Navigate } from 'react-router-dom';
 
 export default class Register extends Component {
+    constructor(props) {
+        //Call the constructor of Super class i.e The Component
+        super(props);
+        //maintain the state required for this component
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+            redirect: '',
+        };
+        // //Bind the handlers to this class
+        this.firstNameChangeHandler = this.firstNameChangeHandler.bind(this);
+        this.lastNameChangeHandler = this.lastNameChangeHandler.bind(this);
+        this.emailChangeHandler = this.emailChangeHandler.bind(this);
+        this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+    }
+    componentDidMount() {
+        const { history } = this.props;
+    }
+    firstNameChangeHandler = (e) => {
+        this.setState({
+            first_name: e.target.value,
+        });
+    };
+    lastNameChangeHandler = (e) => {
+        this.setState({
+            last_name: e.target.value,
+        });
+    };
+    emailChangeHandler = (e) => {
+        this.setState({
+            email: e.target.value,
+        });
+    };
+    passwordChangeHandler = (e) => {
+        this.setState({
+            password: e.target.value,
+        });
+    };
+    submit = (e) => {
+        console.log('SUBMITTING');
+        // store state variables in local storage
+        localStorage.setItem('firstName', this.state.first_name);
+        localStorage.setItem('lastName', this.state.last_name);
+        localStorage.setItem('email', this.state.email);
+        localStorage.setItem('password', this.state.password);
+        // logic to go to next page
+        this.setState({
+            redirect: <Navigate to='/registerUserInfo' replace={true} />,
+        });
+    };
+    // 3rd page:
+    // submit = async (e) => {
+    //     // create payload dictionary
+    //     // populate payload dictionary: eg: payload['firstName'] = localStorage.getItem("firstName")
+    //     // send to backend: const response = await axios.post("URL", payload)
+    //     // take further action: redirect to next page
+    // }
     render() {
+        console.log('RENDERING');
         return (
             <div>
+                {this.state.redirect}
                 <Container className='mt-3'>
-                    <Form>
-                        <Form.Group className='mb-3' controlId='formFirstName'>
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control type='firstName' placeholder='Enter First Name' />
-                            {/* <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text> */}
-                        </Form.Group>
-                        <Form.Group className='mb-3' controlId='formLastName'>
-                            <Form.Label>Last Name</Form.Label>
-                            <Form.Control type='lastName' placeholder='Enter Last Name' />
-                        </Form.Group>
-                        <Form.Group className='mb-3' controlId='formEmail'>
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter Email' />
-                            {/* <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text> */}
-                        </Form.Group>
-                        <Form.Group className='mb-3' controlId='formPassword'>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder='Enter Password' />
-                        </Form.Group>
-                        {/* <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+                    {/* <Form> */}
+                    <Form.Group className='mb-3' controlId='formFirstName'>
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type='firstName' placeholder='Enter First Name' onChange={this.firstNameChangeHandler} />
+                        {/* <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text> */}
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='formLastName'>
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type='lastName' placeholder='Enter Last Name' onChange={this.lastNameChangeHandler} />
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='formEmail'>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type='email' placeholder='Enter Email' onChange={this.emailChangeHandler} />
+                        {/* <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text> */}
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='formPassword'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' placeholder='Enter Password' onChange={this.passwordChangeHandler} />
+                    </Form.Group>
+                    {/* <Form.Group className='mb-3' controlId='formBasicCheckbox'>
                             <Form.Check type='checkbox' label='Check me out' />
                         </Form.Group> */}
-                        <Button variant='success' type='submit' className='green-primary-btn'>
-                            Sign Up
-                        </Button>
-                    </Form>
+                    <Button variant='success' type='submit' className='green-primary-btn' onClick={this.submit}>
+                        Sign Up
+                    </Button>
+                    {/* </Form> */}
                 </Container>
             </div>
         );
