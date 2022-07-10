30680cbcdd14d07e30a5236d3b2df448108a71c9
Dhruvin Shah <dhruvinshah12@gmail.com>
2022-07-09 17:18:15 -0700

    fix (backend): validate registered user api

--- backend/src/modules/user/controller/users.js
+++ backend/src/modules/user/controller/users.js
@@ -24,7 +24,22 @@ export class UserController {
             console.error(err);
         }
     };
-
+    validateRegisteredUser = async (req, res) => {
+        try {
+            const email = req.query.email;
+            console.log(email);
+            userSchema.findOne({ email }).then((user) => {
+                console.log(user);
+                if (!user) {
+                    return res.status(200).json({ message: 'Success, No user found' });
+                } else {
+                    return res.send({ message: 'User Already Exist' });
+                }
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
     register = async (req, res) => {
         userSchema.findOne({ email: req.body.email }).then((user) => {
             if (user) {
