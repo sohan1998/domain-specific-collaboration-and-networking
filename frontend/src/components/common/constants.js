38fbad289c57446c892e76bda60d78cc69b4fcac
sohan1998 <sohan1998@gmail.com>
2022-07-21 19:36:49 -0700

    fix (frontend): update constants

--- frontend/src/components/common/constants.js
+++ frontend/src/components/common/constants.js
@@ -3,7 +3,7 @@ export const frontendPort = 3000;
 export const sessionSecretKey = 'CMPE_295_A_B';
 export const backendIP = 'localhost';
 export const backendPort = 3001;
-export const flaskIP = 'localhost';
+export const flaskIP = '127.0.0.1';
 export const flaskPort = 5000;
 
 // module.exports = { frontendIP, frontendPort, sessionSecretKey };
