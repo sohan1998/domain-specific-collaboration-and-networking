a5da339b637fb939b51739c648d3962564d59f54
sohan1998 <sohan1998@gmail.com>
2022-07-26 21:52:42 -0700

    feat (frontend): handle logout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { CookiesProvider } from 'react-cookie';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <CookiesProvider>
+                <App />
+            </CookiesProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
